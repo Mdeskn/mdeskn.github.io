a627877d13911642f110a89c04e243fcf5cc1b7f
mdeskn <maed.es74@gmail.com>
2024-03-16 01:28:07 +0330

    UPDATE

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// Define topFunction globally
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 $(document).ready(function() {
 
-
     // Assign active class to nav links while scrolling
     $(window).scroll(function() {
         var scrollDistance = $(window).scrollTop();
-
-        $('section').each(function(i) {
-            if ($(this).position().top <= scrollDistance && $(this).position().top + $(this).outerHeight() > scrollDistance) {
-                $('.navbar-nav a.active').removeClass('active');
-                $('.navbar-nav a').eq(i).addClass('active');
-            }
-        });
 
         // Get the button
         let mybutton = document.getElementById("myBtn");
